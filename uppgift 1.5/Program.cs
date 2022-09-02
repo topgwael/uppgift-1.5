3cc986636500391964f6a7fcaa51437050d1c903
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-02 14:34:08 +0200

    Add project files.

---
+++ uppgift 1.5/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Uppgift_1._5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är Nästan Klar med Kapitel 1");
+
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och / ");
+
+
+
+
+
+        }
+
+    }
+}
+
+  
